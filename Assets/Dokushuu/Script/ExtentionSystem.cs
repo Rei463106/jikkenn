bda681c1b3769ae60771c03484af922dcaaa33c6
mayarei2662 <mayarei2662@gmail.com>
2026-05-22 18:31:38 +0900

    [Add]拡張メソッド

--- Assets/Dokushuu/Script/ExtentionSystem.cs
+++ Assets/Dokushuu/Script/ExtentionSystem.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
+/// <summary>
+/// 拡張メソッド
+/// </summary>
 public class ExtentionSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //今回はSystem.Stringクラスに機能を追加する
+    private string t = "ありがとう";
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Debug.Log(t.Repeat(3));
+    }
+}
+
+//staticクラスにする
+internal static class StringExtentions
+{
+    //staticを入れる
+    //this 拡張したいクラス　…の二点を入れる
+    public static string Repeat(this string str, int count)
+    {
+        var builder = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            builder.Append(str);
+        }
+        return builder.ToString();
     }
 }
